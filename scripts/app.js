1ae7b66b21459fa871fe6fb20a78214c83b27b69
trevorwrightdev <trevor.wright.710@gmail.com>
2021-11-16 14:08:14 -0700

    Finished the main enu

--- scripts/app.js
+++ scripts/app.js
@@ -1,2 +1,15 @@
-console.log('sanity check');
+const gameMenu = $('.grid-container');
+
+gameMenu.remove();
+
+const button = $('button');
+
+button.click(function (e) { 
+    e.preventDefault();
+    
+    $('.main-menu').fadeOut();
+
+    button.off('click');
+
+});
 
